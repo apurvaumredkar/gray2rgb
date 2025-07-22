6a74b3b4a485f4b51938d8e4c9d7668dd333891e
Apurva Umredkar <apoorv.umredkar@outlook.com>
2025-07-22 04:22:13 -0400

    updated evaluation script to remove LPIPS as it was too computationally expensive. ran evaluation and saved results on the latest training (10 epochs, 25000 training samples, batch size 8, 128x128 px resolution)

--- evaluate.py
+++ evaluate.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 from skimage.metrics import structural_similarity as compare_ssim
 from torchvision.utils import save_image
-import lpips
+from tqdm import tqdm
 from model import UNetViT
 
 
@@ -61,17 +61,11 @@ def ssim_metric(pred, target):
     ssim = compare_ssim(
         target_np,
         pred_np,
-        channel_axis=2,  # Use this instead of multichannel=True
+        channel_axis=2,
         data_range=1.0,
-        win_size=7  # default is 7, can adjust if needed
+        win_size=7
     )
     return ssim
-
-
-def lpips_metric(lpips_fn, pred, target):
-    pred_norm = pred * 2 - 1
-    target_norm = target * 2 - 1
-    return lpips_fn(pred_norm.unsqueeze(0).to(device), target_norm.unsqueeze(0).to(device)).item()
 
 
 def save_prediction(pred_tensor, save_path):
@@ -82,17 +76,16 @@ def evaluate(model, test_loader, device, save_dir="./predictions", metrics_json=
     os.makedirs(save_dir, exist_ok=True)
 
     model.eval()
-    lpips_fn = lpips.LPIPS(net='alex').to(device)
 
     metrics = {
         "MAE": [],
         "PSNR": [],
-        "SSIM": [],
-        "LPIPS": []
+        "SSIM": []
     }
 
     with torch.no_grad():
-        for gray, rgb, filenames in test_loader:
+        progress_bar = tqdm(test_loader, desc="Evaluating", leave=True)
+        for gray, rgb, filenames in progress_bar:
             gray, rgb = gray.to(device), rgb.to(device)
             preds = model(gray)
 
@@ -100,14 +93,12 @@ def evaluate(model, test_loader, device, save_dir="./predictions", metrics_json=
                 metrics["MAE"].append(mae_metric(pred, target))
                 metrics["PSNR"].append(psnr_metric(pred, target))
                 metrics["SSIM"].append(ssim_metric(pred, target))
-                metrics["LPIPS"].append(lpips_metric(lpips_fn, pred, target))
 
                 save_path = os.path.join(save_dir, fname)
                 save_prediction(pred, save_path)
 
     avg_metrics = {k: float(np.mean(v)) for k, v in metrics.items()}
 
-    # Save to JSON
     with open(metrics_json, "w") as f:
         json.dump(avg_metrics, f, indent=4)
 
@@ -122,7 +113,7 @@ def main():
     test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
     model = UNetViT().to(device)
-    checkpoint_path = "checkpoints/model_e1_20250722_021134.pt"  # Update path accordingly
+    checkpoint_path = "checkpoints/model_e10_20250722_040306.pt"  # Update as needed
     model.load_state_dict(torch.load(checkpoint_path, map_location=device))
 
     metrics = evaluate(model, test_loader, device)
